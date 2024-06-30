c224ed4a9fb676b117e6b925b769404d5ccb7b81
Larissa <63319533+ltsilva23@users.noreply.github.com>
2024-06-30 18:45:37 -0300

    Update main.py

--- workout_api/main.py
+++ workout_api/main.py
@@ -1,5 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi_pagination import add_pagination
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import IntegrityError
 from workout_api.routers import api_router
 
 app = FastAPI(title='WorkoutApi')
+
+@app.exception_handler(IntegrityError)
+async def integrity_error_handler(request: Request, exc: IntegrityError):
+    if 'atleta' in str(exc.orig):
+        return JSONResponse(
+            status_code=303,
+            content={"detail": f"Já existe um atleta cadastrado com o cpf: {request.json()['cpf']}"}
+        )
+    return JSONResponse(
+        status_code=400,
+        content={"detail": "Erro de integridade de dados"}
+    )
+
 app.include_router(api_router)
+add_pagination(app)
